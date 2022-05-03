ceec11f0e07d53183eec0072f0bbd9608d88e3d8
Evelyn-ch <evelynchajud@hotmail.com>
2022-05-03 10:41:55 -0300

    REACT-INTRO

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-export default function Card(props) {
+
+
+export default function Card(props){
+
   // acá va tu código
-  return <div>Card Component</div>
+  var URLimagen = `http://openweathermap.org/img/wn/${props.img}@2x.png`
+  return <div>
+    <button onClick = {props.onClose}> x</button>  
+      <h1>{props.name}</h1>,
+        <div>
+            <h3>Max {props.max}</h3>
+            <h3> Min{props.min}</h3>
+            <h3> {props.img}</h3>
+            <img src= {URLimagen} alt="imagen climatica" />
+      </div>
+</div>
 };
